1c7bb5766994a241c2474c711da260d5fa2968a2
charancholaveti <cholaveticharan47@gmail.com>
2024-12-19 21:55:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css'
+import Alert from './components/Alert';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React,{useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+function App() {
+  const[mode,setMode]=useState('light');
+  const[alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500)
+  }
+ 
+  const handleToggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='black';
+      showAlert("Dark Mode has been enabled","success");
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light Mode has been enabled","success");
+    }
+  }
+  return (
+    <>
+    <Router>
+              <Navbar title="TextUtils" about="About" mode={mode} toggleMode={handleToggleMode}/>
+              <Alert alert={alert}/>
+              <div className="container">
+                    <Routes>   
+                        <Route exact path="/about" element={<About />} />
+                        <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" ShowAlert={showAlert} mode={mode}/>} />
+                   </Routes>
+              </div> 
+             
+   </Router>
+   
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,88 @@
+import React,{useState} from 'react'
+export default function TextForm(props) {
+    const handleOnChange=(event)=>{
+        setText(event.target.value);
+    }
+    const handleUpClick=()=>{
+        let newText=text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to Uppercase","success");
+    }
+    const handleLoClick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to Lowercase","success");
+    }
+    const handleClearClick=()=>{
+        let newText='';
+        setText(newText);
+        props.showAlert("Text is cleared","success");
+    }
+    const handleCamelClick=()=>{
+        let ans=text.toLowerCase();
+        let newTxt=ans.split(" ").reduce((s, c) => s + (" "+c.charAt(0).toUpperCase() + c.slice(1)));
+        setText(newTxt);
+        props.showAlert("Converted to Camelcase","success");
+    }
+    const handleTitleClick = ()=>{
+        let newTxt=text.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+        setText(newTxt);
+        props.showAlert("Converted to Titlecase","success");
+    }
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+        props.showAlert("Text copied to clipboard","success");
+    }
+    const handleExtraSpace=()=>{
+        let newtxt=text.split(/[ ]+/);
+        setText(newtxt.join(' '));
+        props.showAlert("Extra space has been removed","success");
+    }
+    const handleAlternatingCase = () => {
+        let newtext = ""
+        for (let index = 0; index < text.length; index++) {
+            if ((index % 2) === 0) {
+                newtext += text[index].toLowerCase()
+            }
+            else {
+                newtext += text[index].toUpperCase()
+            }
+
+        }
+        setText(newtext);
+        props.showAlert("Converted to Alternatingcase","success");
+    }
+    const[text,setText]=useState('');
+  return (
+    <>
+        <div className="container my-3"style={{color:props.mode==='light'?'black':'white'}}>
+                <div className="mb-3">
+                <h2>{props.heading}</h2>
+                <textarea className="form-control"  value={text} onChange={handleOnChange} id="myBox" rows="6" 
+                style={{backgroundColor:props.mode==='light'?'white':'black',color:props.mode==='light'?'black':'white'}}></textarea>
+                </div>
+                <center>
+                <button className="btn btn-outline-info mx-1"style={{width:'200px'}} onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-outline-info mx-1" style={{width:'200px'} }onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-outline-info mx-1" style={{width:'200px'}} onClick={handleCamelClick}>Convert to Camelcase</button>
+                <button className="btn btn-outline-info mx-1" style={{width:'200px'}} onClick={handleTitleClick}>Convert to Titlecase</button>
+                <br></br>
+                <br></br>
+                <button className="btn btn-outline-info mx-1" style={{width:'200px'}} onClick={handleAlternatingCase}>Alternating Case</button>
+                <button className="btn btn-outline-info mx-1" style={{width:'200px'}} onClick={handleExtraSpace}>Remove Extra Spaces</button>
+                <button className="btn btn-outline-info mx-1" style={{width:'200px'}} onClick={handleClearClick}>Clear text</button>
+                <button className="btn btn-outline-info mx-1" style={{width:'200px'}} onClick={handleCopy}>Copy text</button>
+                </center>
+               
+
+       </div>
+       <div className="container my-3"style={{color:props.mode==='light'?'black':'white'}}>
+        <h3>Your Text Summary:</h3>
+        <p><i>Word Count</i> : {text.split(' ').length} <b>|</b> <i>Character Count</i> : {text.length} <b>|</b> <i>Time to read</i>  : {0.008 * text.split(' ').length} minutes  </p>
+        {/* <p>Your text requires {0.008 * text.split(' ').length} minutes to read</p> */}
+        <h3>Preview</h3>
+        <p>{text.length>0?text:"Enter something in the above text box to preview it here"}</p>
+       </div>
+    </>
+  )
+}

8a963c8db5067461b5c8685fc81319699a6bf711
charancholaveti <cholaveticharan47@gmail.com>
2024-12-25 15:32:29 +0530

    express validator added

--- cloudnest/backend/routes/auth.js
+++ cloudnest/backend/routes/auth.js
@@ -1,14 +1,61 @@
-const express = require('express');
+// const express = require('express');
 
-const router =express.Router();
+// const router =express.Router();
 
-const User=require('../models/User');
+// const User=require('../models/User');
 
-router.post('/',(req,res)=>{
-   console.log(req.body);
-   const user=User(req.body);
-   user.save();
-   res.send(req.body);
-})
+// const { body, validationResult } = require('express-validator');
 
-module.exports=router;
+// router.post('/',[
+//     body('name',"Enter a valid name").isLength({ min: 5 }),
+//     body('email',"Enter a valid Email").isEmail(),
+//     body('password',"Password length must be greater than 5").isLength({ min: 5 }),
+// ],(req,res)=>{
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+//     User.create({
+//         name: req.body.name,
+//         password: req.body.password,
+//         email: req.body.email
+//       }).then(user => res.json(user));
+
+   
+// })
+
+// module.exports=router;
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+// Create a user using POST "/api/auth", doesn't require authentication
+router.post('/', [
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', "Enter a valid Email").isEmail(),
+  body('password', 'Password must have a minimum of 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+    res.json(user);
+  } catch (error) {
+    if (error.code === 11000) {
+      // Duplicate key error
+      return res.status(400).json({ error: 'Email already exists' ,message:error.message});
+    }
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+module.exports = router;

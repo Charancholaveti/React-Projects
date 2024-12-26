236971bb717b58a679270d510ce4307112f5f7be
charancholaveti <cholaveticharan47@gmail.com>
2024-12-26 17:39:05 +0530

    auth token added

--- cloudnest/backend/routes/auth.js
+++ cloudnest/backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { data } = require("react-router-dom");
+
+const JWT_Secret="ch@r@nCholaveti";
 
 // Create a user using POST "/api/auth/createUser", doesn't require authentication
 router.post('/createUser', [
@@ -22,13 +27,23 @@ router.post('/createUser', [
     if(user){
         return res.status(400).json({ error:'Sorry user with this email already exists'})    
     }
+    const salt = await bcrypt.genSaltSync(10);
+    const secPass= await bcrypt.hash(req.body.password,salt)
     //creating a new user
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password
+      password:secPass
     });
-    res.json(user);
+
+    const data={
+      user:{
+        id:user.id
+      }
+    }
+
+    const authToken=jwt.sign(data,JWT_Secret);
+    res.json({authToken});
   } catch (error) {
     if (error.code === 11000) {
       // Duplicate key error

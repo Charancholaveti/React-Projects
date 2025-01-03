5fa65496059a8035889b9f1fbd05480786732ad5
charancholaveti <cholaveticharan47@gmail.com>
2025-01-03 11:45:46 +0530

    navbar and shop added

---
+++ reduxapp/src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='container'>
+    <h2>Withdraw & Deposit</h2>
+      <button className="btn btn-primary mx-2">+</button>
+        Your Balance
+      <button className="btn btn-primary mx-2">-</button>
+    </div>
+  )
+}
+
+export default Shop
